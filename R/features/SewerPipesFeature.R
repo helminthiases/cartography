57ad44b2720ce6235ad37a3c8a88311bac0ad6a5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-30 16:14:06 +0100

    Update R/features/SewerPipesFeature.R: Re-write in progress

--- R/features/SewerPipesFeature.R
+++ R/features/SewerPipesFeature.R
@@ -13,31 +13,12 @@ source(file = 'R/functions/Unlink.R')
 #'
 #' @param year: The sewer pipes year of interest
 #'
-SewerPipesFeature <- function (year) {
-
-
-  # A source example
-  url <- paste0('https://cloud.ihme.washington.edu/s/bkH2X2tFQMejMxy/download?',
-                'path=%2FS_PIPED%20-%20Access%20to%20sewer%20and%20septic%20sanitation%20facilities%20%5BGeoTIFF%5D%2FPercent&files=')
-  item <- 'IHME_LMIC_WASH_2000_2017_S_PIPED_PERCENT_UPPER_'
-  date <- '_Y2020M06D02.TIF'
-
-
-  # Prepare
-  path <- file.path(getwd(), 'data', 'shapes', 'WASH', 'sewer', paste0(item, year, date))
-  UnlinkFiles(path = path)
-
-
-  # Unload map data
-  httr::GET(url = paste0(url, item, year, date),
-            httr::write_disk(path = path),
-            overwrite = TRUE)
-
+SewerPipesFeature <- function (path, item, year, affix) {
 
   # Read the map data
   # class(map)
   # cat(terra::crs(map))
-  map <- terra::rast(file.path(getwd(), 'data', 'shapes', 'WASH', 'sewer', paste0(item, year, date)))
+  map <- terra::rast(file.path(path, paste0(item, year, affix)))
 
 
   return(map)
