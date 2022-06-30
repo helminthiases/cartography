dc4639d5ccfbaecd3d7aa58acd2cffd3489ddbf5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-30 15:10:00 +0100

    Add R/src/interface.R

---
+++ R/src/interface.R
@@ -0,0 +1,38 @@
+# Title     : interface.R
+# Objective : Interface
+# Created by: greyhypotheses
+# Created on: 30/06/2022
+
+
+
+# The end point & affix of the cloud data
+endpoint <- 'https://cloud.ihme.washington.edu/s/bkH2X2tFQMejMxy/download?'
+affix <- '_Y2020M06D02.TIF'
+
+
+
+# The sewer data of interest
+paths <- c('%2FS_IMP%20-%20Access%20to%20any%20improved%20sanitation%20facility%20%5BGeoTIFF%5D%2FPercent',
+           '%2FS_IMP_OTHER%20-%20Access%20to%20a%20non-piped%20improved%20sanitation%20facility%20%5BGeoTIFF%5D%2FPercent',
+           '%2FS_OD%20-%20Reliance%20on%20open%20defecation%20%5BGeoTIFF%5D%2FPercent',
+           '%2FS_PIPED%20-%20Access%20to%20sewer%20and%20septic%20sanitation%20facilities%20%5BGeoTIFF%5D%2FPercent',
+           '%2FS_UNIMP%20-%20Reliance%20on%20unimproved%20sanitation%20facilities%20%5BGeoTIFF%5D%2FPercent')
+
+items <- c('IHME_LMIC_WASH_2000_2017_S_IMP_PERCENT_MEAN_', 'IHME_LMIC_WASH_2000_2017_S_IMP_OTHER_PERCENT_MEAN_',
+           'IHME_LMIC_WASH_2000_2017_S_OD_PERCENT_MEAN_', 'IHME_LMIC_WASH_2000_2017_S_PIPED_PERCENT_MEAN_',
+           'IHME_LMIC_WASH_2000_2017_S_UNIMP_PERCENT_MEAN_')
+
+variables <- c('improved', 'unpiped', 'surface', 'piped', 'unimproved')
+
+
+
+
+case <- function (path, item, variable) {
+
+  root <- paste0(endpoint, 'path=', path, '&files=')
+  storage <- file.path(getwd(), 'data', 'WASH', 'sewer', variable)
+
+  
+}
+mapply(FUN = case, path = paths, item = items, variable = variables)
+
