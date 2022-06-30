81fbe4f41f175b6bdf4be270a0ff80154dfd6dce
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-06-30 21:29:33 +0100

    Add R/features/AddVariables.R

---
+++ R/features/AddVariables.R
@@ -0,0 +1,47 @@
+# Title     : AddVariables.R
+# Objective : Add variables
+# Created by: greyhypotheses
+# Created on: 30/06/2022
+
+
+source(file = 'R/features/PointMapping.R')
+
+
+#' A mind boggling investigation in progress
+#'
+#' @param experiment: An experiments data set
+#' @param mapstring: must point to either /data/WASH/sewer/... or /data/WASH/water/...
+#' @param variable: either improved, piped, surface, unimproved, or unpiped
+#' @param affix:
+#'
+AddVariables <- function (experiment, mapstring, variable, affix) {
+
+
+  # The years of the experiments data set
+  years <- unique(experiment$year)
+  years <- years[years %in% seq(from = 2000, to = 2017)]
+
+
+  # IHME WASH
+  case <- function(year, experiment, mapstring, variable, affix) {
+
+    # map path
+    path <- file.path(mapstring, year, affix)
+
+    # the feature values at a coordiate point
+    derivations <- PointMapping(experiment, path, year)
+    names(derivations) <- c('id', variable)
+
+    return(derivations)
+  }
+
+  estimates <- mapply(FUN = case,
+                      year = years,
+                      MoreArgs = list(experiment = experiment, mapstring = mapstring, variable = variable, affix = affix))
+
+  estimates <- dplyr::bind_rows(estimates)
+
+  return(estimates)
+
+
+}
