ad6ba554a39b54f5f538c4ba0500c9defc7eb5ef
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-09 04:14:55 +0100

    Update R/features/WASH/Hygiene.R

--- R/features/WASH/Hygiene.R
+++ R/features/WASH/Hygiene.R
@@ -4,6 +4,7 @@
 # Created on: 04/07/2022
 
 
+
 #' IHME Hygiene Features
 #'
 #' @param file: The path to a STH experments data file that includes longitude & latitude coordinates
@@ -17,8 +18,8 @@
 Hygiene <- function (file, variables, items, types, repo, affix, storage) {
 
 
-  # the features function
-  source(file = 'R/features/WASH/FetchVariables.R')
+  # the features add function
+  source(file = 'R/features/WASH/AddVariables.R')
 
 
   # the experiments data set in focus
@@ -27,36 +28,11 @@ Hygiene <- function (file, variables, items, types, repo, affix, storage) {
   period <- seq(from = 2000, to = 2017)
 
 
-  # per WASH variable, this function will return the variable estimate per observation,
-  # using the observation's geographic coordinates and its experiment year
-  case <- function (variable, item, type, experiment, repo, affix) {
-    estimates <- FetchVariables(experiment = experiment, mapstring = file.path(repo, type, variable, item),
-                                name = paste0(variable, '_', type), affix = affix)
-    return(list(estimates))
-  }
-
-
   # the function is applicable to observations wherein the experiment year ∈ [2000 2017]
   if (sum(years %in% period) > 0) {
-
-    factors <- mapply(FUN = case, variable = variables,
-                      item = items,
-                      type = types,
-                      MoreArgs = list(experiment = experiment, repo = repo, affix = affix))
-    factors <- dplyr::bind_cols(factors)
-
-    frame <- base::merge(x = experiment, y = factors, by = 0, all.x = TRUE, sort = FALSE)
-    frame <- base::subset(x = frame, select = -Row.names)
-    str(frame)
-
-    utils::write.table(x = frame,
-                       file = file.path(storage, base::basename(path = file)),
-                       append = FALSE,
-                       sep = ',',
-                       row.names = FALSE,
-                       col.names = TRUE,
-                       fileEncoding = 'UTF-8')
-
+    AddVariables(experiment = experiment, variables = variables, items = items,
+                 types = types, repo = repo, affix = affix, storage = storage)
   }
+
 
 }
