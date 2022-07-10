6d36aa99d2a2828fcdb44834e98a43aff6c96da5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-10 21:33:01 +0100

    Update R/features/population/interface.R

--- R/features/population/interface.R
+++ R/features/population/interface.R
@@ -4,17 +4,13 @@
 # Created on: 07/07/2022
 
 
-source(file = 'R/features/population/PointMapping.R')
-source(file = 'R/features/population/Interpolating.R')
+
 
 
 # the list of experiment files
-files <- list.files(path = file.path(getwd(), 'warehouse', 'ESPEN', 'experiments', 'extended'),
+files <- list.files(path = file.path(getwd(), 'warehouse', 'WASH'),
                     full.names = TRUE)
 
-
-# an experiments file
-frame <- read.csv(file = files[3])
 
 
 # arguments
@@ -23,20 +19,4 @@ root <- file.path(getwd(), 'data', 'population')
 affix <- '_30s.tif'
 
 
-# per observation, via its coordinates, the population density estimates per map year
-estimates <- mapply(FUN = PointMapping, year = years, MoreArgs = list(root = root, affix = affix, frame = frame))
-estimates <- dplyr::bind_rows(estimates)
-row.names(estimates) <- NULL
 
-# In <estimates> each observation of <frame> is associated with population density
-# estimates for the years {2000, 2005, 2010, 2015, 2020}.  Remember, the observation
-# code is field <id>.  The function Interpolating(.) estimates the population
-# densities of the intervening years.
-interpolations <- mapply(FUN = Interpolating, id = unique(estimates$id),
-                         MoreArgs = list(estimates = estimates, years = years))
-interpolations <- dplyr::bind_rows(interpolations)
-row.names(interpolations) <- NULL
-interpolations <- rename(interpolations, 'p_density' = 'estimate')
-
-# finally, appending a population density field to <frame>
-extended <- dplyr::left_join(x = frame, y = interpolations, by = c('id', 'year'))
