03aa27cd50f998fd15b5222b2b29dfd53772ebe5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-10 21:32:47 +0100

    Update R/features/WASH/interface.R

--- R/features/WASH/interface.R
+++ R/features/WASH/interface.R
@@ -31,7 +31,7 @@ affix <- '_Y2020M06D02.TIF'
 
 
 # storage
-storage <- file.path(getwd(), 'warehouse', 'WASH')
+storage <- file.path(getwd(), 'warehouse', 'features', 'WASH')
 if (dir.exists(storage)) {
   base::unlink(x = storage, recursive = TRUE)
 }
