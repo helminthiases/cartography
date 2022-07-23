c008f0626636048ef0bec5decd68c4410d7a390f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-07-23 19:08:11 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -17,22 +17,3 @@ source(file = 'R/features/population/interface.R')
 source(file = 'R/features/elevation/interface.R')
 
 
-# external storage
-external <- file.path(base::dirname(getwd()), 'modelling', 'data')
-if (dir.exists(external)) {
-  base::unlink(x = external, recursive = TRUE)
-}
-dir.create(path = external, recursive = TRUE)
-
-src <- list.files(path = file.path(getwd(), 'warehouse', 'features', 'elevation'),
-                   full.names = TRUE)
-dst <- lapply(X = src, FUN = function (x) file.path(external, base::basename(x)))
-dst <- unlist(dst)
-
-# in parallel
-cores <- parallel::detectCores()/2
-doParallel::registerDoParallel(cores = cores)
-clusters <- parallel::makeCluster(cores)
-parallel::clusterMap(clusters, fun = file.copy, src, dst)
-parallel::stopCluster(clusters)
-rm(clusters, cores)
