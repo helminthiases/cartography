d74618ed08ac2ecb29f80738a5e6f02f120ea063
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-04 18:53:52 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -4,8 +4,10 @@
 # Created on: 29/06/2022
 
 
-# Unload IHME WASH maps
-# source(file = 'R/src/WASH/interface.R')
+# Data
+source(file = 'R/src/miscellaneous')
+source(file = 'R/src/population/interface.R')
+source(file = 'R/src/WASH/interface.R')
 
 # Add IHME WASH features to each STH experiments data file
 source(file = 'R/features/WASH/interface.R')
