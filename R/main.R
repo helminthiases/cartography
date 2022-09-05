e8cf2f711d8ee6f12b6253a7c61dd10dcf7c7cc1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-05 01:26:46 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -23,19 +23,16 @@ source(file = 'R/features/population/interface.R')
 # Add elevation
 source(file = 'R/features/elevation/interface.R')
 
-# Export
-source(file = 'R/export.R')
 
 
-# the <url> list of the enhanced data files
+# The <url> list of the enhanced data files
 files <- list.files(path = file.path(getwd(), 'warehouse', 'features', 'data'), full.names = TRUE)
 files <- lapply(X = files, FUN = function (x) unlist(base::strsplit(x = x, split = 'spatial'))[2]) %>%
   unlist()
 URL <- lapply(X = files, FUN = function (x) paste0('https://raw.githubusercontent.com/helminthiases/spatial/master', x)) %>%
   unlist()
 
-
-# saving the <url> list of enhanced data files
+# ... saving the <url> list of enhanced data files
 utils::write.table(x = data.frame(path = URL), file = file.path(getwd(), 'warehouse', 'features', 'data.csv'),
                    append = FALSE, sep = ',', na = '',
                    row.names = FALSE, col.names = TRUE, fileEncoding = 'UTF-8')
